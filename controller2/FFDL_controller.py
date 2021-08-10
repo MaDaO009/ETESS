145f0e2fd38b7fae9f0a241717d811370a95defb
MaDaO009 <40014763+MaDaO009@users.noreply.github.com>
2021-08-10 18:50:30 +0800

    update

--- controller2/FFDL_controller.py
+++ controller2/FFDL_controller.py
@@ -7,15 +7,16 @@ class FFDL_controller:
         self.N=1
         self.sample_time=sample_time
     def init(self):
-        self.heading_controllers=[PID(P=0.5,I=0.1,D=0.2,sample_time=self.sample_time) for i in range(self.N)]
+        self.heading_controllers=[PID(P=0.5,I=0.1,D=0.2,sample_time=self.sample_time,maximum=0.7,minimum=-0.7) for i in range(self.N)]
         self.v_controller=FFDLcController(Nt=500,le=1,lu=2,yd_init=2,ys_init=[0,0,0,0])
 
     def update_state(self,temp,new_poses,twists):
         self.poses=new_poses    
-        rudder_commands=[self.heading_controllers[i].update(new_poses[i][3],1.57) for i in range(self.N)]
+        rudder_commands=[-self.heading_controllers[i].update(new_poses[i][3],1.57) for i in range(self.N)]
+        # print(rudder_commands)
         thruster_commands=self.v_controller.compute_u(twists[0][0],[float(twists[i][0]) for i in range(1,self.N)])
         thruster_commands=[float('{0:.3f}'.format(i[0])) for i in thruster_commands]
-        thruster_commands=[0.3]+thruster_commands
+        thruster_commands=[0.8]+thruster_commands
         # print(twists,thruster_commands)
         # print([[rudder_commands[i],thruster_commands[i]] for i in range(self.N)])
         return [[rudder_commands[i],thruster_commands[i]] for i in range(self.N)]
--- demo4.py
+++ demo4.py
@@ -2,6 +2,6 @@ from simulator import simulator
 from controller2.FFDL_controller import FFDL_controller
 import math
 my_controllers=FFDL_controller()
-a=simulator(my_controllers,config_file="config/default4.txt")
+a=simulator(my_controllers,config_file="config/default2.txt")
 a.run()
 # a.replay("./data/test",0.02)
